628a618d0c975efee1af1cbb94b06b995b9d6bb6
Derek Cerretani <derekcerretani@gmail.com>
2018-12-19 18:30:49 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -62,8 +62,7 @@ def runner
   # code runner here
   welcome
   initial_round
-  until card_total > 21
-    hit?(card_total)
+  until hit?(card_total) > 21
     display_card_total(card_total)
   end
   end_game
